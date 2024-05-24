1f9d9501f895ad4a45ffb8e94a2dfe4830a17cc6
keybian <keybian@gmail.com>
2024-05-24 10:15:34 -0400

    Configuracion inicial

---
+++ incatein/src/main/java/com/kp/incatein/IncateinApplication.java
@@ -0,0 +1,13 @@
+package com.kp.incatein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncateinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncateinApplication.class, args);
+	}
+
+}
